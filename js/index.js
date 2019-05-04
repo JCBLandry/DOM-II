eba8274ec06a26b878bdcdf175933c99b79828e3
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-05-04 11:32:40 -0500

    Base Event Listeners added!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+let busimg = document.getElementById("busimg");
+console.log(busimg);
+
+busimg.addEventListener('mouseover', function(e){
+console.log('imghovered!')
+});
+
+document.addEventListener('scroll', function(e){
+    console.log('scrolled!')
+});
+
+busimg.addEventListener('click', function(e){
+console.log('clicked!')
+});
+
+document.addEventListener('keydown', function(e){
+    console.log('key pressed!')
+});
+
+document.addEventListener('keyup', function(e){
+    console.log('key let go!')
+});
+
+document.addEventListener('cut', function(e){
+    alert('whatcha cuttin on?')
+});
+
+document.addEventListener('copy', function(e){
+    alert('whatcha stealin??')
+});
+
+document.addEventListener('resize', function(e){
+    alert('resized!')
+});
+
+document.addEventListener('funnscreenchange', function(e){
+    alert('fullscreen shift!')
+});
+
+document.addEventListener('paste', function(e){
+    alert('dont leave that here!')
+});
